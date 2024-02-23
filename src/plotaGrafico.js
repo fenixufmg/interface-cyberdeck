6f2bdd88b6342299f98fad2ec5f70d4e35582692
Igor Martins <iguinhoigor4@gmail.com>
2024-02-23 20:17:52 -0300

    feat: Adicionar gráficos

--- src/plotaGrafico.js
+++ src/plotaGrafico.js
@@ -1,64 +1,49 @@
-import Chart from "chart.js/auto";
+// Função para criar gráfico usando Chart.js
+function createChart(containerId) {
+  const container = document.getElementById(containerId);
+  const canvas = document.createElement("canvas");
+  container.appendChild(canvas);
 
-document.addEventListener("DOMContentLoaded", function () {
-  // Criação de gráficos para cada div específica
-  const acelerometroChart = createChart("acelerometro", "Acelerômetro");
-  const altimetroChart = createChart("altimetro", "Altímetro");
-  const giroscopioChart = createChart("giroscopio", "Giroscópio");
-  const velocimetroChart = createChart("velocimetro", "Velocímetro");
-
-  function createChart(containerId, label) {
-    const container = document.getElementById(containerId);
-    const canvas = document.createElement("canvas");
-    container.appendChild(canvas);
-
-    const ctx = canvas.getContext("2d");
-    return new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: [], // Adicione seus rótulos de tempo aqui
-        datasets: [
-          {
-            label: label,
-            data: [],
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 2,
-            fill: false,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+  const ctx = canvas.getContext("2d");
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          data: [],
+          borderColor: "#282c34",
+          borderWidth: 2,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
         },
       },
-    });
-  }
+      plugins: {
+      },
+    },
+  });
+}
 
-  function updateChart(chart, newDataPoint) {
-    // Atualiza o gráfico com novos dados
-    chart.data.labels.push(new Date().toLocaleTimeString()); // Adicione rótulo de tempo atual
-    chart.data.datasets[0].data.push(newDataPoint); // Adicione novo ponto de dados
-    chart.update();
-  }
+// Função para atualizar gráfico com novos dados
+function updateChart(chart, newDataPoint) {
+  chart.data.labels.push(new Date().toLocaleTimeString());
+  chart.data.datasets[0].data.push(newDataPoint);
+  chart.update();
+}
 
-  // Simulação de dados para os gráficos (substitua pelos seus dados reais)
-  setInterval(
-    () => updateChart(acelerometroChart, Math.floor(Math.random() * 50) + 10),
-    5000
-  );
-  setInterval(
-    () => updateChart(altimetroChart, Math.floor(Math.random() * 50) + 10),
-    5000
-  );
-  setInterval(
-    () => updateChart(giroscopioChart, Math.floor(Math.random() * 50) + 10),
-    5000
-  );
-  setInterval(
-    () => updateChart(velocimetroChart, Math.floor(Math.random() * 50) + 10),
-    5000
-  );
-});
+// Simulação de dados para os gráficos (substitua pelos seus dados reais)
+const acelerometroChart = createChart("acelerometroChart");
+const altimetroChart = createChart("altimetroChart");
+const giroscopioChart = createChart("giroscopioChart");
+const velocimetroChart = createChart("velocimetroChart");
+
+setInterval(() => updateChart(acelerometroChart, Math.floor(Math.random() * 50) + 10), 50);
+setInterval(() => updateChart(altimetroChart, Math.floor(Math.random() * 60) + 10), 50);
+setInterval(() => updateChart(giroscopioChart, Math.floor(Math.random() * 70) + 10), 50);
+setInterval(() => updateChart(velocimetroChart, Math.floor(Math.random() * 80) + 10), 50);
